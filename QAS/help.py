f85f4ccdd6bf2eb03a8eae4d42ff25d9d6dc2c2d
EricMac <ericmac@EricMac.local>
2023-03-01 09:22:14 -0800

    set up both existing python environments

--- QAS/help.py
+++ QAS/help.py
@@ -4,5 +4,9 @@ Hello and welcome to QAS, Quality Assurance Strategies.
 This project is for experimenting with different testing strategies and to identify underlying behaviors
 found in the tools needed for the trade of Quality Assurance Engineer.
 
-PyTest serves as the base for this project, and hopefully shows expertise in its usage.
+PyTest is the main framework in Python we use to control testing, so we leverage this as needed.
+
+Venv is crucial to our work because we need to keep environments clean when testing.
+
+
 """)
